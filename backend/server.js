16bd58d930783c808e2b593588b21976294849b6
SudiptaSaha123 <sudiptasaha883@gmail.com>
2025-05-25 14:09:41 +0530

    cors configuration modified

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Configure CORS with specific options
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: '*',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
